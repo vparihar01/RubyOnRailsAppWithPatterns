59706135b449a06c8257144419e0fd756382c6b8
Vivek Parihar <vivek@weboniselab.com>
2013-04-27 15:42:34 +0530

    application setup done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3BootstrapDeviseCancan::Application.config.secret_token = '80e25fd1868ed8bc484aa6da7866329eeadd37056718d9da98c15b4a476ded5844f861f0450c2f718239e3d44593888cd34d926bed1dae655a12fa51ea638865'
